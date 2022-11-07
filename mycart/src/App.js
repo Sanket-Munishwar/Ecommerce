43f0408d8c90892f0e002b08f16aab6bdc43944e
Sanket Munishwar <sanket@techis.io>
2022-11-07 19:15:07 +0530

    update

---
+++ mycart/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import Product from './components/Product';
+import {  Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+   <>
+      <Header/>
+   
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/product' element={<Product />} />
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/contact' element={<Contact />} />
+            
+          </Routes>
+     
+      
+
+   </>
+      
+    
+  );
+}
+
+export default App;
---
+++ mycart/src/Data.jsx
@@ -0,0 +1,22 @@
+const DATA = [
+{
+    id : 0,
+    title : "Snx0",
+    price : 100,
+    img: "/assets/images/products/img1.jpeg"
+},
+{
+    id : 1,
+    title : "Snx01",
+    price : 110,
+    img: "/assets/images/home/img2.jpeg"
+},
+{
+    id : 2,
+    title : "Snx02",
+    price : 130,
+    img: "/assets/images/products/img3.jpeg"
+},
+
+]
+
